57980d2ba2a37775caed216814a7fc8069fc3fca
Master-Tech271 <umar17605@gmail.com>
2020-03-17 16:10:09 +0530

    remove alert

--- application/views/admin/showDetails_Intern.php
+++ application/views/admin/showDetails_Intern.php
@@ -122,7 +122,6 @@
   $(document).ready(function(){
     //to deapproved
     $(document).on('click', '.disapproved', function(){
-      alert($(this).attr('id'));
       Swal.fire({
   title: 'Are you sure?',
   text: "You won't be able to revert this!",
