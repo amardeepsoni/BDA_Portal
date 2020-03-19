f40ffe4390660dad135fffeb46a73f6ed9bf64bb
amar soni <amardeep.soni88@gmail.com>
2020-03-19 17:19:07 +0530

    popup

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,6 +4,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Home extends CI_Controller {
 
 	public function index() {
+		if ($this->session->userdata('intern')) {
+			redirect('intern/dashboard');
+		}
 		$data['page_title'] = 'Home';
 		$this->load->View('header', $data);
 		$this->load->View('home');
