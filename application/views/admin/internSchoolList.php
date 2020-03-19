4ba2b2afbceac84f0853bdafd363a9c00ba799b3
Master-Tech271 <umar17605@gmail.com>
2020-03-19 09:19:33 +0530

    delete unused code like comment code

--- application/views/admin/internSchoolList.php
+++ application/views/admin/internSchoolList.php
@@ -44,11 +44,7 @@
     </tr>
   </tbody>
 </table>
-<div class="row">
-<div class="col-12">
-<p class="text-center font-weight-bold" style="word-spacing: 30px;"><?php //$this->pagination->create_links();?></p>
-</div>
-</div>
+
 </div>
 </div>
 </div>
